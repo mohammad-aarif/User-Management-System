cc63b2d065906b493361812e92299c573abf1edf
mohammad-aarif <arifbd354@gmail.com>
2025-02-25 21:43:33 +0600

    error page added!

--- src/Component/Error Page/ErrorPage.jsx
+++ src/Component/Error Page/ErrorPage.jsx
@@ -1,7 +1,11 @@
+import { NavLink } from "react-router-dom";
+
 const ErrorPage = () => {
     return (
-        <div>
-            Error page!@
+        <div className="flex flex-col items-center max-h-full my-16">
+            <h1 className="text-8xl text-amber-400">404</h1>
+            <p className="text-3xl text-gray-300">Page Not Found!</p>
+            <NavLink className='btn bg-amber-400 my-2 px-8 py-1 rounded-b-sm hover:bg-amber-500' to='/'>Home</NavLink>
         </div>
     );
 };
